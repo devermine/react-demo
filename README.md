33fb44cbbfd7beb02009d902cdc801c693a65f0d
ARTEM SITNIKOV <artemsitnikov@ARTEMs-MacBook-Pro.local>
2015-08-18 19:10:27 +0400

    add comments

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Install: `npm install`
 Start webpack dev server http://react.local:9090: `npm run dev`
 
 Build: `npm run deploy`
+For production mode add main css file in index.html head `<link rel="stylesheet" href="/assets/bundle.css">`
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+//extract-text-webpack-plugin - потребуется для сборки всех css в один файл
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var Webpack = require('webpack');
-
+/*
+ При сборке проекта, не важно в каком режиме, всегда определена переменная BUILD_DEV см. package.json
+ При помощи этой переменной, можно определить в каком режиме осуществляется сборка и в зависимости от этого
+ произвести нужные действия
+ */
 var development = JSON.parse(process.env.BUILD_DEV || 1);
-
+//Если потребуется в скриптах/модулях знать в каком режиме работает приложение, то можно использовать переменную __DEV__
 var definePlugin = new Webpack.DefinePlugin({
 	__DEV__: development
 });
@@ -10,20 +15,33 @@ var definePlugin = new Webpack.DefinePlugin({
 module.exports = {
 
 	entry: {
+		//для работы в режеми development в качестве входной точки так же нужно указать webpack/hot/dev-server
 		app: development ? ["webpack/hot/dev-server", "./app/main.js"] : ["./app/main.js"]
 	},
 	output: {
+		//все собранные файлы будут сохраняться в path
 		path: __dirname + '/assets',
 		publicPath: '/assets/',
+		//название файла, в которому будет объеденины все js файлы
 		filename: 'bundle.js'       
 	},
 	module: {
 		loaders: [
 			{
+				/*
+				 компоненты reactjs написаны с использованием JSX разметки и классов, которые доступны только в es6,
+				 чтобы итоговый js был понят браузерами, нужно привести его в нормальный вид, за это отвечает транспайлер babel,
+				 который преобразует jsx разметку в обычный js, а так же код стандарта es6 преобразует в код стандарта es5
+				 */
+
 				test: /\.js$/,
 				exclude: /(node_modules)/, 
 				loader: "uglify!babel-loader"
 			},
+			/*
+			 css файлы при сборки проекта в продакшн режиме объединяются в один файл bundle.css
+			 В режиме разработки все css файлы будут загружены на страницу в тег style
+			 */
 			{ 
 				test: /\.min\.css/,
 				loader: development ? "style-loader!css-loader" : ExtractTextPlugin.extract("css-loader")
